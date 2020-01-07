2d32703a0461bab773ae33bede7078474b4bfc59
Markiian Matsiuk <matsiuk@ucu.edu.ua>
2020-01-07 14:13:11 +0200

    Done fat prsing and output formatting

--- includes/utils.h
+++ includes/utils.h
@@ -65,19 +65,101 @@ namespace FAT16 {
 }
 
 
-void printFileDescription(FAT16::file & file) {
+std::string timeParse(std::bitset<16> b) {
+    std::stringstream ss;
+
+    int parsed = 0;
+    for (int i = 11; i <= 15; ++i) {
+        if (b[i]) {
+            parsed += 1 << (i-11);
+        }
+    }
+    ss << std::setfill('0') << std::setw(2) << parsed << ":";
+
+    parsed = 0;
+    for (int i = 5; i <= 10; ++i) {
+        if (b[i]) {
+            parsed += 1 << (i-5);
+        }
+    }
+    ss << std::setfill('0') << std::setw(2) << parsed << ":";
+
+    parsed = 0;
+    for (int i = 0; i <= 4; ++i) {
+        if (b[i]) {
+            parsed += 1 << i;
+        }
+    }
+    ss << std::setfill('0') << std::setw(2) << parsed*2;
+
+    return ss.str();
+}
+
+
+std::string dateParse(std::bitset<16> b) {
+    std::stringstream ss;
+
+    int parsed = 0;
+    for (int i = 0; i <= 4; ++i) {
+        if (b[i]) {
+            parsed += 1 << i;
+        }
+    }
+    ss << parsed << ".";
+
+    parsed = 0;
+    for (int i = 5; i <= 8; ++i) {
+        if (b[i]) {
+            parsed += 1 << (i-5);
+        }
+    }
+    ss << parsed << ".";
+
+    parsed = 0;
+    for (int i = 9; i <= 15; ++i) {
+        if (b[i]) {
+            parsed += 1 << (i - 9);
+        }
+    }
+    ss << 1980+parsed;
+
+    return ss.str();
+}
+
+std::string attributeParse(std::bitset<8> b, std::map<uint8_t, std::string> map) {
+    std::stringstream ss;
+    for ( auto & entry : map) {
+//        std::cout << "\n===\n" << b << "\n" << std::bitset<8>(entry.first) << "\n===\n";
+        auto attrBitset = std::bitset<8>(entry.first);
+        if ((b & attrBitset).count() == attrBitset.count()) {
+            ss << entry.second;
+        } else {
+            ss << "    ";
+        }
+    }
+
+//    ss << std::endl<< b  << std::endl;
+//    ss << std::endl<< (b & std::bitset<8>(0x01 )).count() << std::endl;
+
+    return ss.str();
+}
+
+void printFileDescription(FAT16::file & file, std::map<uint8_t, std::string> map) {
     std::stringstream ss;
     ss << file.filename;
     if (file.size == 0) {
         ss << "        <dir>";
     } else {
-        ss << "." << file.ext << "    " << std::setfill(' ') << std::setw(5) <<file.size;
+        ss << "." << file.ext << "    " << std::setfill(' ') << std::setw(5) <<file.size ;
     }
-    ss << "   " << std::bitset< 8 >( file.attr ) << "    " << file.address << std::endl;
-    std::cout << ss.str();
+    ss << "    " << std::setfill(' ') << std::setw(10) << dateParse(std::bitset<16>(file.fileModifiedDate)) << " ";
+    ss << timeParse(std::bitset<16>(file.fileModifiedTimeHMS)) ;
+    ss << "   "  << attributeParse( file.attr , map) << "    ";
+    ss << std::setfill(' ') << std::setw(5) << file.address << "    "<< std::endl;
 
 //    std::cout << std::bitset<16>(file.fileModifiedDate) << std::endl;
-//    std::cout << std::bitset<16>(file.fileModifiedDate ) << std::endl;
+//    std::cout << bitsetPart<10,16>(std::bitset<16>(file.fileModifiedDate))<< std::endl;
+    std::cout << ss.str();
 
 }
 #endif //FAT_UTILS_H
--- main.cpp
+++ main.cpp
@@ -13,19 +13,27 @@
 //std::string s = std::bitset< 64 >( 12345 ).to_string(); // string conversion
 //std::cout << std::bitset< 64 >( 54321 ) << ' '; // direct output
 
-int main() {
+int main(int argc, char **argv) {
     std::map<uint8_t, std::string> attributeNames;
-    attributeNames[0x01] = std::string{"Read only"};
-    attributeNames[0x02] = std::string{"Hidden file"};
-    attributeNames[0x04] = std::string{"System file"};
-    attributeNames[0x08] = std::string{"Volume label"};
-    attributeNames[0x0f] = std::string{"Long file name"};
-    attributeNames[0x10] = std::string{"Directiry"};
-    attributeNames[0x20] = std::string{"Archive"};
+    attributeNames[0x01] = std::string{" Ro "};
+    attributeNames[0x02] = std::string{" Hf "};
+    attributeNames[0x04] = std::string{" Sf "};
+    attributeNames[0x08] = std::string{" Vl "};
+    attributeNames[0x0f] = std::string{"LFN "};
+    attributeNames[0x10] = std::string{" Di "};
+    attributeNames[0x20] = std::string{" Ar "};
 
-    int fd = open("/home/markiian/Downloads/hd0_just_FAT16_without_MBR.img", O_RDONLY);
+    if (argc!=2) {
+        std::cout << "Incorrect number of arguments, usage:\n    FATParser [filename]"<< std::endl;
+        return 1;
+    }
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        std::cerr << "Cannot open file: " << argv[1]  << std::endl;
+        return 1;
+    }
 
-    void * dataBootSector = mmap(NULL, BOOT_SECOR_SIZE, PROT_READ, MAP_PRIVATE, fd, 0);
+    void * dataBootSector = mmap(nullptr, BOOT_SECOR_SIZE, PROT_READ, MAP_PRIVATE, fd, 0);
     RawData imageBootSector{dataBootSector, BOOT_SECOR_SIZE};
     FAT16::bootSector bootSector(imageBootSector);
     munmap(dataBootSector, BOOT_SECOR_SIZE);
@@ -46,7 +54,7 @@ int main() {
     size_t desiredOffset = BOOT_SECOR_SIZE + bootSector.FATNum * bootSector.FATSize * bootSector.bytesPerSector;
     size_t validOffset = (desiredOffset / pageSize) * pageSize;
     size_t additionalOffset = desiredOffset - validOffset;
-    void * dataRootDir = mmap(NULL,
+    void * dataRootDir = mmap(nullptr,
                               bootSector.rootDirCapacity * DIR_ENTRY_SIZE + additionalOffset,
                               PROT_READ, MAP_PRIVATE, fd,
                               validOffset);
@@ -57,7 +65,7 @@ int main() {
         if (file.status == 0x00) {
             break;
         }
-        printFileDescription(file);
+        printFileDescription(file, attributeNames);
     }
 
     munmap(dataRootDir, bootSector.rootDirCapacity * DIR_ENTRY_SIZE + additionalOffset);
